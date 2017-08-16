8517be5ec9c816e345919c16162cf164f33ee2fa
Stephanie Concepcion <contact@stephanieconcepcion.com>
2017-08-16 11:48:20 -0400

    alter render function for fade in effect

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const images = ['illo1.png', 'illo2.png', 'illo3.png', 'illo4.png', 'illo5.png', 'illo6.png', 'illo7.png', 'illo8.png', 'illo9.png', 'illo10.png', 'illo11.png', 'illo12.png', 'illo13.png', 'illo14.png', 'illo15.png', 'illo16.png'];
 
-// display all images files on page
-function renderGallery(files) {
+// display all images files on page with fade in effect
+function fadeInRenderGallery(files) {
   const galleryDiv = document.querySelector('.gallery');
   const path = 'images/thumbs/';
-  let count = 0;
+  let time = 70;
 
   files.forEach(file => {
     const image = document.createElement('img');
+
     image.src = `${path}${file}`;
+    image.style.opacity = 0;
     galleryDiv.appendChild(image);
-    count++;
+
+    setTimeout(() => {
+      image.style.opacity = 1;
+    }, time);
+
+    time += 70;
   });
 
-  console.log(count);
 }
 
-renderGallery(images);
+fadeInRenderGallery(images);
