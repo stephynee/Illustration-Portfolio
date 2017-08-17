784a091559f786bcac14c3b28fc2855612041530
Stephanie Concepcion <contact@stephanieconcepcion.com>
2017-08-17 11:45:18 -0400

    style lightbox overlay

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const images = ['illo1.png', 'illo2.png', 'illo3.png', 'illo4.png', 'illo5.png', 'illo6.png', 'illo7.png', 'illo8.png', 'illo9.png', 'illo10.png', 'illo11.png', 'illo12.png', 'illo13.png', 'illo14.png', 'illo15.png', 'illo16.png'];
+const gallery = document.querySelector('.gallery');
 
 // display all images files on page with fade in effect
 function fadeInRenderGallery(files) {
@@ -10,7 +11,9 @@ function fadeInRenderGallery(files) {
     const image = document.createElement('img');
 
     image.src = `${path}${file}`;
+    image.dataset.filename = file;
     image.style.opacity = 0;
+
     galleryDiv.appendChild(image);
 
     setTimeout(() => {
@@ -19,7 +22,15 @@ function fadeInRenderGallery(files) {
 
     time += 70;
   });
+}
 
+// display correct image in lightbox when gallery is clicked.
+function showLightBox(e) {
+  const {filename} = e.target.dataset;
+
+  // console.log(`image/artwork/${fileName}`);
 }
 
 fadeInRenderGallery(images);
+
+gallery.addEventListener('click', showLightBox);
