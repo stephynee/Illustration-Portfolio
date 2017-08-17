31f74b8fcf3b57b7a8ebd82598fb5dc28a548e47
Stephanie Concepcion <contact@stephanieconcepcion.com>
2017-08-17 12:08:51 -0400

    add function for displaying lightbox

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const images = ['illo1.png', 'illo2.png', 'illo3.png', 'illo4.png', 'illo5.png', 'illo6.png', 'illo7.png', 'illo8.png', 'illo9.png', 'illo10.png', 'illo11.png', 'illo12.png', 'illo13.png', 'illo14.png', 'illo15.png', 'illo16.png'];
 const gallery = document.querySelector('.gallery');
+const body = document.querySelector('body');
 
 // display all images files on page with fade in effect
 function fadeInRenderGallery(files) {
@@ -25,12 +26,31 @@ function fadeInRenderGallery(files) {
 }
 
 // display correct image in lightbox when gallery is clicked.
-function showLightBox(e) {
+function showLightBox(e, el) {
   const {filename} = e.target.dataset;
+  const path = 'images/artwork/';
+  const lightbox = document.createElement('div');
+  const img = document.createElement('img');
+  const exit = document.createElement('i');
 
-  // console.log(`image/artwork/${fileName}`);
+  img.src = `${path}${filename}`;
+
+  exit.classList.add('fa');
+  exit.classList.add('fa-times');
+  exit.addEventListener('click', closeLightbox);
+
+  lightbox.classList.add('lightbox');
+  lightbox.appendChild(exit);
+  lightbox.appendChild(img);
+
+  el.appendChild(lightbox);
+}
+
+// remove the lightbox from DOM
+function closeLightbox() {
+
 }
 
 fadeInRenderGallery(images);
 
-gallery.addEventListener('click', showLightBox);
+gallery.addEventListener('click', e => showLightBox(e, body));
